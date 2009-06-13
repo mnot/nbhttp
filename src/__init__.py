72fd612b268b76b629c9afe92010ffead68653b0
Mark Nottingham <mnot@mnot.net>
2009-06-14 09:15:04 +1000

    add dummy handler

--- src/__init__.py
+++ src/__init__.py
@@ -6,4 +6,7 @@ from push_tcp import run, stop, schedule
 def header_dict(header_tuple, strip=None):
     if strip == None:
         strip = []
-    return dict([(n.strip().lower(),v.strip()) for (n,v) in header_tuple])
+    return dict([(n.strip().lower(), v.strip()) for (n,v) in header_tuple])
+
+def dummy(*args, **kw):
+    pass
