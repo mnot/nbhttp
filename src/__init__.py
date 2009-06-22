9112c0a40973e6094775aa2830cb8cd67e52ce87
Mark Nottingham <mnot@mnot.net>
2009-06-22 21:26:21 +1000

    include get_hdr

--- src/__init__.py
+++ src/__init__.py
@@ -7,4 +7,4 @@ Non-blocking HTTP components.
 from client import Client
 from server import Server
 from push_tcp import run, stop, schedule
-from common import dummy, header_dict
+from common import dummy, header_dict, get_hdr
