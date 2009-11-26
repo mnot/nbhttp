c116b8982658ba188ccaa9be28fbb53b5ddb2cc1
Mark Nottingham <mnot@mnot.net>
2009-11-26 20:44:56 +1100

    rename http files back to normal

--- src/__init__.py
+++ src/__init__.py
@@ -4,8 +4,8 @@
 Non-blocking HTTP components.
 """
 
-from http_client import Client
-from http_server import Server
+from client import Client
+from server import Server
 from push_tcp import run, stop, schedule
 from http_common import dummy, header_dict, get_hdr, \
     safe_methods, idempotent_methods, hop_by_hop_hdrs
