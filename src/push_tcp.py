c9dfcdc2c38ffd0f2d9e892ee3886380dda47288
Mark Nottingham <mnot@mnot.net>
2009-11-26 20:22:55 +1100

    make server connect error handler a bit more explanatory

--- src/push_tcp.py
+++ src/push_tcp.py
@@ -326,7 +326,7 @@ class create_server(asyncore.dispatcher):
         tcp_conn = _TcpConnection(conn, self.host, self.port, self.handle_error)
         tcp_conn.read_cb, tcp_conn.close_cb, tcp_conn.pause_cb = self.conn_handler(tcp_conn)
 
-    def handle_error(self, err):
+    def handle_error(self, err=None):
         raise AssertionError, "this (%s) should never happen for a server." % err
 
 
