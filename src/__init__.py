9c0bd61a5599ae2a36e644998bed54e2f710f147
Mark Nottingham <mnot@mnot.net>
2010-08-24 11:59:45 +1000

    make now available at package level

--- src/__init__.py
+++ src/__init__.py
@@ -6,6 +6,6 @@ Non-blocking HTTP components.
 
 from client import Client
 from server import Server
-from push_tcp import run, stop, schedule
+from push_tcp import run, stop, schedule, now
 from http_common import dummy, header_dict, get_hdr, \
     safe_methods, idempotent_methods, hop_by_hop_hdrs
