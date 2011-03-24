f3fb2458e2b32e883e5bc16a0300d31b33019282
Mark Nottingham <mnot@mnot.net>
2011-03-24 17:46:05 +1100

    bump version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='nbhttp',
-      version='0.7.1',
+      version='0.7.2',
       description='Non-blocking HTTP components',
       author='Mark Nottingham',
       author_email='mnot@mnot.net',
       url='http://github.com/mnot/nbhttp/',
-      download_url='http://github.com/mnot/nbhttp/tarball/nbhttp-0.7.1',
+      download_url='http://github.com/mnot/nbhttp/tarball/nbhttp-0.7.2',
       packages=['nbhttp'],
       package_dir={'nbhttp': 'src'},
       scripts=['scripts/proxy.py'],
